36d2ca5a588ef599730461751f7b582940d0fca7
vedantlbhatt <vedubhatt2006@gmail.com>
2025-12-09 15:13:16 -0500

    base call set up with Twilio

---
+++ make_call.py
@@ -0,0 +1,18 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+    twiml="<Response><Say>Sigmamundo!</Say></Response>",
+    to="+14043331778",
+    from_="+18668961216",
+)
+
+print(call.sid)
+
